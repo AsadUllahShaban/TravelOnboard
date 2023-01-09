b7cb9e4a4fa0ac4b4c725b3caae88cc32ca00f38
AsadUllahShaban <asadullahshaban1997@gmail.com>
2023-01-10 01:31:08 +0500

    Onboarding pics added

--- app/screens/OnBoarding/OnBoarding.js
+++ app/screens/OnBoarding/OnBoarding.js
@@ -1,13 +1,72 @@
-import {Text, View, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
-import {COLORS} from '../../constants';
+import {
+  ScrollView,
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Animated,
+  Image,
+} from 'react-native';
+// import {COLORS} from '../../constants';
+
+//constants
+import {images, theme} from '../../constants';
+const {onbaording1, onbaording2, onbaording3} = images;
+
+// theme
+const {COLORS, FONTS, SIZES} = theme;
+
+// Dummy Data
+const onBoardings = [
+  {
+    title: "Let's Travelling",
+    description:
+      'Just one small positive thought in the morning can change your whole day',
+    img: onbaording1,
+  },
+  {
+    title: 'Navigation',
+    description: "Opportunities don't happen, you create them",
+    img: onbaording2,
+  },
+  {
+    title: 'Destination',
+    description: 'It is never too late to be what you might have been',
+    img: onbaording3,
+  },
+];
 
 // export default function OnBoarding() {
 const OnBoarding = () => {
+  //Render
+  function renderContent() {
+    return (
+      <Animated.ScrollView
+        horizontal
+        pagingEnabled
+        scrollEnabled
+        snapToAlignment={'center'}>
+        {onBoardings.map((item, index) => (
+          <View key={index} style={{width: SIZES.width}}>
+            <View
+              style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={item.img}
+                resizeMode="cover"
+                style={{
+                  width: '100%',
+                  height: '100%',
+                }}
+              />
+            </View>
+          </View>
+        ))}
+      </Animated.ScrollView>
+    );
+  }
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Onboarding Screen</Text>
-    </SafeAreaView>
+    <SafeAreaView style={styles.container}>{renderContent()}</SafeAreaView>
   );
 };
 
